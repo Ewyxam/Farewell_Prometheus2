37f94a4fd41f9877f4f61f8fedc7b290709cd29f
Maxime <maxime.debock@gmail.com>
2020-02-26 16:35:11 +0100

    ColliderResized

---
+++ FarewellPrometheus/Assets/Scripts/EventAnim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventAnim : MonoBehaviour
+{
+    public GameObject launchscan;
+    public void EndAnim()
+    {
+        gameObject.GetComponent<Animator>().SetBool("load", false);
+        launchscan.GetComponent<DescriptionManager>().LaunchScan();
+    }
+    
+}

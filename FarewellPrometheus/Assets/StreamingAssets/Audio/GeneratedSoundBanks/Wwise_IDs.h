915936e439fb6586b72d9578443ff3bd4a3b090e
Antonin Terrien <anto.terrien@gmail.com>
2020-02-26 13:46:04 +0100

    Son can et statut vaisseau

--- FarewellPrometheus/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ FarewellPrometheus/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -19,6 +19,7 @@ namespace AK
         static const AkUniqueID BUTTON_HOVER = 1701177942U;
         static const AkUniqueID CALENDAR_HANDLE = 2242625264U;
         static const AkUniqueID COMPUTER_SCAN = 1307820710U;
+        static const AkUniqueID COMPUTER_SHIP_STATUS_EVENT = 3118182193U;
         static const AkUniqueID COMPUTER_START = 2806985083U;
         static const AkUniqueID MUSIC_EVENT = 55565095U;
         static const AkUniqueID PASSWORD_RIGHT = 2063851173U;
@@ -196,6 +197,7 @@ namespace AK
     namespace AUX_BUSSES
     {
         static const AkUniqueID GR1_REVERB = 1325898718U;
+        static const AkUniqueID VS1_REVERB = 574621144U;
     } // namespace AUX_BUSSES
 
     namespace AUDIO_DEVICES

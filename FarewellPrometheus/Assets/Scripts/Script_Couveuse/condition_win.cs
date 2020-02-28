76b0a8201a3fc562fd5d65b98293ec99666c71a7
Matt <a199577@gmail.com>
2020-02-28 14:32:45 +0100

    Couveuse package chargé

---
+++ FarewellPrometheus/Assets/Scripts/Script_Couveuse/condition_win.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class condition_win : MonoBehaviour
+{
+    public bool winCondition1 = false;
+    private float positionInitialeX;
+    private float positionInitialeY;
+
+  /*  void Start()
+    {
+        Vector3 position1 = transform.position;
+        positionInitialeX = transform.position.x;
+        positionInitialeY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x != positionInitialeX || transform.position.y != positionInitialeY)
+        {
+            verif();
+        }
+
+    }
+
+    private void verif()
+    {
+        if (transform.position.x < 0.5 && transform.position.x > -0.5 && transform.position.y > 1)
+        {
+            Debug.Log("Win Condition 1 est vérifiée");
+            winCondition1 = true;
+        }
+
+    }*/
+}

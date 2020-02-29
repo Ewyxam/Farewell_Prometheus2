8b15790364a7d86cbf61d4f28b5d4b20318c96da
Matt <a199577@gmail.com>
2020-02-29 16:57:50 +0100

    MM maj

---
+++ FarewellPrometheus/Assets/Scripts/PnC/MapDND.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDND : MonoBehaviour                          // attacher se script à l'objet que l'on souhaite deplacer
+{
+
+    private Vector3 screenPoint;
+    private Vector3 initialPos;
+    private Vector3 offset;                                      // permet de crée un décalage entre la souris et l'objet que l'on selectionne
+    private Vector3 curPosition;                                 // position actuelle
+
+
+    void Start()
+    {
+
+
+        
+    }
+    private void Update()
+    {
+        //transform.position = curPosition;
+
+    }
+
+    void OnMouseDown()                                          // il faut ajouter un collider pour que cette méthode fonctionne (ne fonctionne pas sur IOS)
+    {
+        initialPos = gameObject.transform.position;
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = initialPos - Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+        Debug.Log("on clique sur le sprite");
+    }
+    void OnMouseDrag()                                   // il faut ajouter un collider pour que cette méthode fonctionne (ne fonctionne pas sur IOS)
+    {
+        Debug.Log("On déplace la souri");
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+
+    }
+}
--- FarewellPrometheus/Assets/Scripts/PnC/MiniMap.cs
+++ FarewellPrometheus/Assets/Scripts/PnC/MiniMap.cs
@@ -18,43 +18,43 @@ public class MiniMap : MonoBehaviour
     void Start()
     {
         ColMan = GameObject.Find("CollisionManager").GetComponent<CollisionManager>();
-    CL_MM.GetComponent<Image>().enabled = false;
-    JG_MM.GetComponent<Image>().enabled = false;
-    CP_MM.GetComponent<Image>().enabled = false;
-    INT_MM.GetComponent<Image>().enabled = false;
-    RV_MM.GetComponent<Image>().enabled = false;
-    GR_MM.GetComponent<Image>().enabled = false;
+    CL_MM.GetComponent<RawImage>().enabled = false;
+    JG_MM.GetComponent<RawImage>().enabled = false;
+    CP_MM.GetComponent<RawImage>().enabled = false;
+    INT_MM.GetComponent<RawImage>().enabled = false;
+    RV_MM.GetComponent<RawImage>().enabled = false;
+    GR_MM.GetComponent<RawImage>().enabled = false;
 }
     public void MajMiniMap()
     {
         if (ColMan.discovered[0])
         {
-            CL_MM.GetComponent<Image>().enabled = true;
-        } else CL_MM.GetComponent<Image>().enabled = false;
+            CL_MM.GetComponent<RawImage>().enabled = true;
+        } else CL_MM.GetComponent<RawImage>().enabled = false;
         if (ColMan.discovered[1])
         {
-            JG_MM.GetComponent<Image>().enabled = true;
-        } else JG_MM.GetComponent<Image>().enabled = false;
+            JG_MM.GetComponent<RawImage>().enabled = true;
+        } else JG_MM.GetComponent<RawImage>().enabled = false;
         if (ColMan.discovered[2])
         {
-            CP_MM.GetComponent<Image>().enabled = true;
+            CP_MM.GetComponent<RawImage>().enabled = true;
         }
         if (ColMan.discovered[3])
         {
-            INT_MM.GetComponent<Image>().enabled = true;
+            INT_MM.GetComponent<RawImage>().enabled = true;
         }
         if (ColMan.discovered[4])
         {
-            RV_MM.GetComponent<Image>().enabled = true;
+            RV_MM.GetComponent<RawImage>().enabled = true;
         }
         if (ColMan.discovered[5])
         {
-            GR_MM.GetComponent<Image>().enabled = true;
+            GR_MM.GetComponent<RawImage>().enabled = true;
         }
 
         if (ColMan.tabNum == 0)
         {
-            CL_MM.GetComponent<Image>().color = Color.black;
+            CL_MM.GetComponent<RawImage>().color = Color.black;
         } 
         //Debug.Log(ColMan.tabNum);
         //Debug.Log(ColMan.discovered[0]);

1abba21087cb7a8bbcd07f7769e344401946fa29
Matt <a199577@gmail.com>
2020-03-04 09:56:12 +0100

    tableau electrique

---
+++ FarewellPrometheus/Assets/Script/Script_MiniJeu_Electrique/ChangeMaterial.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterial : MonoBehaviour
+{
+
+    public Sprite[] materialLed;
+    SpriteRenderer rend;
+    public bool test = false;
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        rend.enabled = true;
+        rend.sprite = materialLed[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (test)
+        { ChangeCouleur(); }
+    }
+
+    public void ChangeCouleur()
+    {
+        rend.sprite = materialLed[1];
+    }
+}
